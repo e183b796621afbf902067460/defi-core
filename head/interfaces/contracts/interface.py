50e33a7af8239015236f2440f3bc22c8f33d23a7
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2022-08-19 15:37:04 +0300

    Refactored IContract as a Builder

--- head/interfaces/contracts/interface.py
+++ head/interfaces/contracts/interface.py
@@ -7,14 +7,44 @@ class IContract(ABC):
 
     _abi: str = None
 
-    def __init__(self, address: str, provider: BaseProvider) -> None:
-        self.w3 = Web3(provider)
-        self.contract = self.w3.eth.contract(address=Web3.toChecksumAddress(value=address), abi=self.abi)
+    def __init__(self) -> None:
+        self._address = '0x0000000000000000000000000000000000000000'
+        self._provider = None
+        self._contract = None
+
+    def create(self):
+        self._contract = Web3(provider=self.provider).eth.contract(
+            address=Web3.toChecksumAddress(value=self.address), abi=self.abi)
+        return self
 
     @property
-    def address(self) -> str:
-        return self.contract.address
+    def contract(self):
+        return self._contract
 
     @property
     def abi(self) -> str:
         return self._abi
+
+    def getAddress(self) -> str:
+        return self._address
+
+    def setAddress(self, address: str):
+        self._address: str = address
+        return self
+
+    def getProvider(self) -> BaseProvider:
+        return self._provider
+
+    def setProvider(self, provider: BaseProvider):
+        self._provider: BaseProvider = provider
+        return self
+
+    address = property(
+        fget=getAddress,
+        fset=setAddress
+    )
+
+    provider = property(
+        fget=getProvider,
+        fset=setProvider
+    )
